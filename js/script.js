3b475409ea29569fba2eb9a8f22fb6df1ead9f7e
Enrique García Sáez <enrique.garcia@valoraldescubierto.com>
2022-02-09 19:01:53 +0100

    Accessibility pages and styles corrections

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 //Initial menu display
 const iconMenu = document.getElementById('icon-menu');
 const menu = document.getElementById('menu');
-menu.style.display = 'block';
+let toogle = true;
 
 
 
 //Toogle Menu function
 function toogleMenu (){
-    let menuStyle = menu.style.display;
-    if (menuStyle == 'block'){
+    if (toogle == true){
         menu.style.display = 'none';
-    };
-    if (menuStyle == 'none'){
+        toogle = false;
+    } else if (toogle == false){
         menu.style.display = 'block';
+        toogle = true;
     };
 }
 
